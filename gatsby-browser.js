59faf41a488375422fe6e21d78f438ae11f3148e
Staci Shon <staci.shon@gmail.com>
2021-03-21 15:30:15 -0500

    Updates

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-export const onServiceWorkerUpdateReady = () => {
-  // eslint-disable-next-line no-alert
-  const answer = window.confirm(
-    'This application has been updated. Reload to display the latest version?'
-  );
-  if (answer === true) {
-    window.location.reload();
-  }
-};
